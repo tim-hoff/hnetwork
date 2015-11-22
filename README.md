d389623c040137e734a35ad267ecaecef8e3b94b
Tim Hoff <timothy.hoff@praeses.com>
2015-11-21 22:55:11 -0600

    Initial library. Need to clean up and make the math more robust

---
+++ README.md
@@ -0,0 +1,13 @@
+# hnetwork
+
+A Clojure library designed to propigate data through a neural network.
+
+## Usage
+
+FIXME
+
+## License
+
+Copyright © 2015 Timothy Hoff
+
+
---
+++ project.clj
@@ -0,0 +1,10 @@
+(defproject hnetwork "0.1.0"
+  :description "NN stuff"
+  :url "http://timhoff.com/projects/hnetwork"
+  :license {:name "None" }
+  :dependencies [[org.clojure/clojure "1.8.0-beta2"]
+	              [org.clojure/core.async "0.1.346.0-17112a-alpha"]
+								[criterium "0.4.3"]
+	              [net.mikera/vectorz-clj "0.36.0"]
+								[net.mikera/core.matrix "0.42.1"]
+	      				[incanter/incanter "1.9.0"]] )
